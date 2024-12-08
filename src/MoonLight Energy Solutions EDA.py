41401c029f6e7794576a14d9d641672a6081738e
nesredinm <nesredin.m11@gmail.com>
2024-12-08 21:34:15 +0300

    Time series analysis

--- src/MoonLight Energy Solutions EDA.py
+++ src/MoonLight Energy Solutions EDA.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #load the data set
 data = pd.read_csv("D:/AI Mastery/data/benin-malanville.csv")
@@ -8,7 +10,7 @@ data = pd.read_csv("D:/AI Mastery/data/benin-malanville.csv")
 print (data.info())
 print (data.head())
 
-# Generate summary statistics for numeric columns
+"""# Generate summary statistics for numeric columns
 summary_stats = data.describe()
 print("Summary Statistics:\n\n", summary_stats)
 
@@ -24,3 +26,37 @@ std_values = numeric_data.std()
 print("Mean Values:\n", mean_values)
 print("\nMedian Values:\n", median_values)
 print("\nStandard Deviation Values:\n", std_values)
+"""
+
+"""# Check for missing values
+missing_values = data.isnull().sum()
+print("\nMissing Values:\n", missing_values)
+
+# Check for negative values in GHI, DNI, and DHI
+columns_to_check = ['GHI', 'DNI', 'DHI']
+for column in columns_to_check:
+    negative_values = data[data[column] < 0]
+    print(f"\nNegative values in {column}:\n", negative_values)
+
+# Visualize outliers using boxplots for GHI, DNI, and DHI
+
+for column in ['ModA', 'ModB', 'WS', 'WSgust']:
+    plt.figure(figsize=(8, 4))
+    sns.boxplot(x=data[column])
+    plt.title(f"Box Plot of {column}")
+    plt.show()"""
+
+
+# Convert 'Timestamp' to datetime format
+data['Timestamp'] = pd.to_datetime(data['Timestamp'])
+
+# Set the Timestamp column as the index
+data.set_index('Timestamp', inplace=True)
+
+# Plot time series for GHI, DNI, DHI, and Tamb
+columns_to_plot = ['GHI', 'DNI', 'DHI', 'Tamb']
+for column in columns_to_plot:
+    data[column].plot(figsize=(12, 6), title=f"Time Series of {column}")
+    plt.xlabel("Timestamp")
+    plt.ylabel(column)
+    plt.show()
